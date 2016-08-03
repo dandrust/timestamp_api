24d74162cf1022fedcf2db7e050f1593f8e5a991
Dan <dandrust@gmail.com>
2016-08-03 00:03:16 +0000

    completed challenge

--- server.js
+++ server.js
@@ -1,10 +1,36 @@
-var express = require('express')
+var fs = require('fs');
+var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+app.get('/', function(req, res){
+    fs.readFile('index.html', 'utf8', function(err, data){
+        if (err) throw err;
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        res.end(data, 'utf9');
+    });
 });
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 3000!');
+app.get('/:string', function (req, res) {
+  var dateString = req.params.string;
+  var response = {};
+
+  if (!parseInt(dateString, 10)){                       // Input is not a number 
+    var naturalLangDate = new RegExp(/^((January|March|May|July|August|October|December)\s([0-9]|[1-2][0-9]|3[0-1])|(April|June|September|November)\s([0-9]|[1-2][0-9]|30)|February\s([0-9]|1[0-9]|2[0-9]))\,\s[0-9]{4}$/);
+    if (naturalLangDate.test(dateString)) {             // Input is a natural date
+        response['natural'] = dateString;
+        response['unix'] = "" + Date.parse(dateString);
+    } else {                                            // Input is neither a number nor a date
+        response['natural'] = 'null';
+        response['unix'] = 'null';
+    }
+  } else {                                          // Input is a number
+      var dateObj = new Date(parseInt(dateString, 10));
+      var monthsArray = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December" ];
+      response['natural'] = monthsArray[dateObj.getMonth()] + " " + dateObj.getDate() + ", " + dateObj.getFullYear();
+      response['unix'] = dateString;
+  }
+  res.writeHead(200, {'Content-Type': 'application/json'});
+  res.end(JSON.stringify(response));
 });
+
+app.listen(8080);
